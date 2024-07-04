89ba7629857ba86ca90bfe431a4aee13b3919e05
suraj <suraj@itoneclick.com>
2024-07-04 13:30:55 +0530

    dummy png image added

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const http = require('http');
 const express = require('express')
 const app = express()
 
-app.use((req,res,next)=>{
+app.use('/product',(req,res,next)=>{
     console.log("this is form middleware 1")
-    next()
+    res.send("<h1>hello from produnct</h1>")
+
 })
-app.use((req,res,next)=>{
+app.use('/',(req,res,next)=>{
     console.log("this is from middleware 2");
     res.send("<h1>hello from middleware 2</h1>")
 })
